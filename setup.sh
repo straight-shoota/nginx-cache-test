c117307fb6130eb40eafd182534fd57a2d93188a
Johannes Müller <straightshoota@gmail.com>
2017-02-11 16:06:24 +0000

    Add preview_token, README.md and LICENSE

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo ln -sf $(realpath nginx.conf) /etc/nginx/sites-enabled/nginx-cache-test
 
-sudo nginx -t && sudo systemctl reload nginx
+sudo nginx -t && sudo systemctl reload nginx && echo "Nginx restarted"
 
 
--- test.sh
+++ test.sh
@@ -1,8 +1,26 @@
 BACKEND_PORT=8022
 FRONTEND_PORT=8021
 
-frontend="127.0.0.1:${FRONTEND_PORT}/"
+frontend="127.0.0.1:${FRONTEND_PORT}"
+frontend_preview_token="${frontend}?preview_token=123456"
 backend="127.0.0.1:${BACKEND_PORT}/"
+
+function final_stats {
+  echo "bg_counter: ${bg_counter}"
+  echo "fg_counter: ${fg_counter}"
+  echo "cached_counter: ${cached_counter}"
+  echo "bypassed_counter: ${bypassed_counter}"
+  echo "new_bypassed_counter: ${new_cached_counter}"
+  echo "preview_token_counter: ${preview_token_counter}"
+  echo "second_preview_token_counter: ${second_preview_token_counter}"
+}
+
+function fail {
+  echo " ======== FAILED ========"
+  echo "$1"
+  final_stats
+  exit 1
+}
 
 # clear cache
 find cache -type f -delete 
@@ -14,15 +32,13 @@ bg_counter=$(curl -vs "$backend")
 fg_counter=$(curl -vs "$frontend")
 
 if [[ "1" -ne "$(expr $fg_counter - $bg_counter)" ]]; then
-  echo "FAILED: backend counter was $bg_counter, frontend counter $fg_counter"
-  exit 1
+  fail "backend counter was $bg_counter, frontend counter $fg_counter"
 fi
 
 cached_counter=$(curl -vs "$frontend")
 
 if [[ "0" -ne "$(expr $fg_counter - $cached_counter)" ]]; then
-  echo "FAILED: frontend counter $fg_counter was not cached (got $cached_counter)"
-  exit 2
+  fail "frontend counter $fg_counter was not cached (got $cached_counter)"
 else
   echo "=== Counter was successfully retrieved from cache ($cached_counter)"
 fi
@@ -30,8 +46,7 @@ fi
 bypassed_counter=$(curl -vs "$frontend" -H "cachepurge: true")
 
 if [[ "1" -ne "$(expr $bypassed_counter - $fg_counter)" ]]; then
-  echo "FAILED: could not bypass counter (retrieved $bypassed_counter)"
-  exit 3
+  fail "could not bypass counter (retrieved $bypassed_counter)"
 else
   echo "=== Bypass works correctly ($bypassed_counter)"
 fi
@@ -39,12 +54,23 @@ fi
 new_cached_counter=$(curl -vs "$frontend")
 
 if [[ "0" -ne "$(expr $bypassed_counter - $new_cached_counter)" ]]; then
-  echo "FAILED: retrieved a different value on subsequental run: $new_cached_counter"
-  exit 4
+  fail "retrieved a different value on subsequental run: $new_cached_counter"
 fi
 
-echo "bg_counter: ${bg_counter}"
-echo "fg_counter: ${fg_counter}"
-echo "cached_counter: ${cached_counter}"
-echo "bypassed_counter: ${bypassed_counter}"
-echo "new_bypassed_counter: ${new_cached_counter}"
+preview_token_counter=$(curl -vs "$frontend_preview_token")
+
+if [[ "1" -ne "$(expr $preview_token_counter - $new_cached_counter)" ]]; then
+  fail "preview_token counter was cached ($preview_token_counter)"
+fi
+
+second_preview_token_counter=$(curl -vs "$frontend_preview_token")
+
+if [[ "1" -ne "$(expr $second_preview_token_counter - $preview_token_counter)" ]]; then
+  fail "second_preview_token counter was cached ($second_preview_token_counter)"
+fi
+
+echo ""
+echo "=== All tests succeeded ==="
+echo ""
+
+final_stats
